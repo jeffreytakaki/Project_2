d98b144dc196b4179463098f41bcac34e277647b
Jeffrey Takaki <jeffreytakaki@Jeffreys-MacBook-Pro.local>
2014-05-12 18:48:30 -0700

    heroku modified

---
+++ app/controllers/weddings_controller.rb
@@ -0,0 +1,22 @@
+class WeddingsController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
